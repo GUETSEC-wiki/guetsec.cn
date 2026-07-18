6585abb0698241b73d1fad7e7d5cc5931900e1d2
Alice <aaa@alice.com>
2026-07-18 13:35:53 +0800

    26招新文案 视频

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -63,8 +63,8 @@ export default function HomepageFeatures(): JSX.Element {
           ))}
         </div>
         <div>
-          <h1 align="center">2025招新视频</h1>
-          <iframe width="100%" height="720" src="//player.bilibili.com/player.html?bvid=BV18x89zFEQT" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+          <h1 align="center">2026招新视频</h1>
+          <iframe width="100%" height="720" src="//player.bilibili.com/player.html?bvid=BV1oVKA6QEis" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
         </div>
       </div>
     </section>
